aeb5ce56962a356dde82f1da3da2cbdfca4bee4b
aandaleon <angie.andaleon@gmail.com>
2017-05-31 14:33:18 -0500

    Add files via upload

---
+++ convertDosageToGEMMA.R
@@ -0,0 +1,16 @@
+#so this might work
+#jk "Cannot allocate memory"
+"%&%" = function(a,b) paste(a,b,sep="")
+library(data.table)
+for(i in 1:22){
+  path <- "/home/angela/px_cebu_chol/wRelateds/Sanger/Sanger_dosages/chr" %&% i %&% ".maf0.01.info0.8.dosage.txt.gz"
+  genotype <- read.table(path)
+  genotype$V1 <- NULL
+  genotype$V3 <- NULL
+  genotype$V6 <- NULL
+  filename <- "/home/angela/px_cebu_chol/GEMMA/chr" %&% i %&% ".txt"
+  fwrite(genotype, file = filename, row.names = F, quote = F, col.names = F)
+  gzip <- "gzip /home/angela/px_cebu_chol/GEMMA/chr" %&% i %&% ".txt"
+  system(gzip)
+}
+
